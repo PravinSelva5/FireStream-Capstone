0e1281a43761c7544d9f4224ff277df11a52b746
Mithilan Muralitharan <47437389+mithilanm@users.noreply.github.com>
2020-01-18 17:29:03 -0500

    Add files via upload

--- login-page/connect.php
+++ login-page/connect.php
@@ -1,29 +1,36 @@
 <?php
+    session_start();
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (!empty($username) && !empty($password)){
     define('DB_NAME', 'firestream');
     define('DB_USER','root');
     define('DB_PASSWORD','1234');
     define('DB_HOST', 'localhost');
 
-    $link=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+    $link=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or die ("Could not connect!");
 
-    mysqli_select_db($link,DB_NAME);
+    $db=mysqli_select_db($link,DB_NAME) or die ("Could not connect to database!");
+    
 
-    if(isset($_POST['username'])){
-        $uname=$_POST['username'];
-        $password=$_POST['password'];
+    $sql = "SELECT * FROM loginform where User='".$username."'AND Pass='".$password."'limit 1";
 
-        $sql = "SELECT * FROM loginform where User='".$uname."'AND Pass='".$password."'limit 1";
-        
-        $result=mysqli_query($link,$sql);
+    $query = mysqli_query($link,$sql);
+    $numrows = mysqli_num_rows($query);
 
-        if(mysqli_num_rows($result)==1){
-            echo "You have successfully logged in!";
-            exit();
-        }
-        else{
-            echo "You have entered an incorrect password!";
-            exit();
-        }
+    if($numrows === 1){
+        $_SESSION['username'] = $username;
+        header("Location: member.php");
+        exit();
     }
-    mysqli_close();
+    else{
+        $_SESSION['errorMssg'] = "Invalid username or password";
+    }
+    header("Location: index.php");
+    exit();
+}
+else{
+    die("Please enter username and/or password!");
+}
 ?>
---
+++ login-page/member.php
@@ -0,0 +1,4 @@
+<?php
+    ob_start();
+    echo "You have successfully logged in!";
+?>
