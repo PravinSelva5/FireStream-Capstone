0dc0725a35a805ed59cec0db2e9997b98c21ccf9
Mithilan Muralitharan <47437389+mithilanm@users.noreply.github.com>
2020-02-24 18:52:37 -0500

    Add files via upload

---
+++ login-page/content/pi1.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Streaming Pi#1</title>
+        <link rel="stylesheet" href="styles.css" />
+        </head>
+        <body>
+            <iframe width="640" height="480"
+                src="http://172.20.10.5:10000/" scrolling="no">    
+           	</iframe>
+        </body>
+    </html>
---
+++ login-page/content/videos.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Recorded Videos</title>
+        <link rel="stylesheet" href="styles.css" />
+        </head>
+        <body>
+            <p>Hello this page will be for recorded videos</p>
+        </body>
+    </html>
